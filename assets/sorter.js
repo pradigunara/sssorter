3ca8fa7dc2d54778d76094f6af1390af1bd88c25
Prawira Adiguna Pangestu <pradigunara@gmail.com>
2025-05-12 13:28:51 +0700

    use image preloading

--- assets/sorter.js
+++ assets/sorter.js
@@ -200,6 +200,19 @@ function initMemberPic() {
   memberPicId = picSets[rand(0, 1)];
 }
 
+// Preload images
+function preloadImages() {
+  const picSets = [picSet1, picSet2, picSet3, picSet4];
+  for (const picSet of picSets) {
+    for (const member in picSet) {
+      if (picSet.hasOwnProperty(member)) {
+        const img = new Image();
+        img.src = picSet[member];
+      }
+    }
+  }
+}
+
 // Dark mode functionality
 function toggleDarkMode() {
   const body = document.body;
@@ -235,6 +248,9 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".theme-toggle-icon svg").innerHTML =
       '<path d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"></path>';
   }
+
+  // Call preloadImages
+  preloadImages();
 });
 
 var memberColor = {
